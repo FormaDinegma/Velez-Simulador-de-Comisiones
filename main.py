a98075d6c482e074ea8d637844d8f501c77fe111
FormaDinegma <formacion@dinegma.com>
2025-06-26 15:59:58 -0600

    main.py

--- main.py
+++ main.py
@@ -294,7 +294,7 @@ def calcular_comision(tabla, indicador, meta, logro, venta_total):
     return 0
 
 # --- INTERFAZ STREAMLIT ---
-st.markdown("<h1 style='font-size: 40px;'>📊 Simulador de Comisiones <b style='color:#3ECF8E'>Dinegma</b></h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='font-size: 40px;'>📊 Simulador de Comisiones <b style='color:#3ECF8E'>2025</b></h1>", unsafe_allow_html=True)
 
 # 🧍‍♂️ Nombre del colaborador
 nombre = st.text_input("Nombre del asesor o colaborador")
